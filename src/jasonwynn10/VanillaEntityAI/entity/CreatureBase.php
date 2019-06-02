c837921fe2315f37e918eb6c80fa78dc61e4c8f8
jasonwynn10 <jasonwynn10@me.com>
2019-06-02 12:32:17 -0400

    Keep Color and sheared state on restart Add 'Dye' Interaction button

--- src/jasonwynn10/VanillaEntityAI/entity/CreatureBase.php
+++ src/jasonwynn10/VanillaEntityAI/entity/CreatureBase.php
@@ -54,6 +54,7 @@ abstract class CreatureBase extends Creature implements Linkable, Collidable, Lo
 
 	public function initEntity() : void {
 		parent::initEntity();
+		$this->setGenericFlag(self::DATA_FLAG_NO_AI, true);
 	}
 
 	/**
