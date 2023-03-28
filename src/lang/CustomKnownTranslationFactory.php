fa5f69b16dcac128151fd5369848092eecb9a6ff
jasonwynn10 <jasonwynn10@me.com>
2023-03-28 02:32:05 -0400

    generate some language API

---
+++ src/lang/CustomKnownTranslationFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace jasonwynn10\VanillaEntityAI\lang;
+
+use pocketmine\lang\Translatable;
+
+/**
+ * This class contains factory methods for all the translations known to PoliteTeleports.
+ * This class is generated automatically, do NOT modify it by hand.
+ *
+ * @internal
+ */
+final class CustomKnownTranslationFactory{
+	public static function difficulty_command_description() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::DIFFICULTY_COMMAND_DESCRIPTION, []);
+	}
+
+	public static function summon_command_description() : Translatable{
+		return new Translatable(CustomKnownTranslationKeys::SUMMON_COMMAND_DESCRIPTION, []);
+	}
+
+}
---
+++ src/lang/CustomKnownTranslationKeys.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace jasonwynn10\VanillaEntityAI\lang;
+
+/**
+ * This class contains constants for all the translations known to PoliteTeleports.
+ * This class is generated automatically, do NOT modify it by hand.
+ *
+ * @internal
+ */
+final class CustomKnownTranslationKeys{
+	public const DIFFICULTY_COMMAND_DESCRIPTION = "difficulty.command.description";
+	public const SUMMON_COMMAND_DESCRIPTION = "summon.command.description";
+}
