7087f62b3dee64d4f5e0680b1d37ed2c9999da8d
jasonwynn10 <jasonwynn10@me.com>
2023-04-09 23:26:40 -0400

    Register language keys in server language

--- src/Main.php
+++ src/Main.php
@@ -9,8 +9,10 @@ use jasonwynn10\VanillaEntityAI\command\SummonCommand;
 use jasonwynn10\VanillaEntityAI\event\EventListener;
 use jasonwynn10\VanillaEntityAI\event\TaskCleanupListener;
 use pocketmine\entity\EntityFactory;
+use pocketmine\lang\Language;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\SingletonTrait;
+use Symfony\Component\Filesystem\Path;
 
 final class Main extends PluginBase{
 	use SingletonTrait{
@@ -38,5 +40,56 @@ final class Main extends PluginBase{
 
 		new EventListener($this);
 		new TaskCleanupListener($this);
+
+		$this->saveResource('/lang/config.yml', true);
+		/** @var string[][] $contents */
+		$contents = yaml_parse_file(Path::join($this->getDataFolder(), "lang", 'config.yml'));
+		$languageAliases = [];
+		foreach($contents as $language => $aliases){
+			$mini = mb_strtolower($aliases['mini']);
+			$this->saveResource('/lang/data/' . $mini . '.ini', true);
+			$languageAliases[$mini] = $language;
+		}
+
+		$languages = [];
+		$dir = scandir(Path::join($this->getDataFolder(), "lang", "data"));
+		if($dir !== false){
+			foreach($dir as $file){
+				/** @phpstan-var array{dirname: string, basename: string, extension?: string, filename: string} $fileData */
+				$fileData = pathinfo($file);
+				if(!isset($fileData["extension"]) || $fileData["extension"] !== "ini"){
+					continue;
+				}
+				$languageName = mb_strtolower($fileData["filename"]);
+				$language = new Language(
+					$languageName,
+					Path::join($this->getDataFolder(), "lang", "data")
+				);
+				$languages[$languageName] = $language;
+				foreach($languageAliases as $mini => $full){
+					if(mb_strtolower($full) === $languageName){
+						$languages[mb_strtolower($mini)] = $language;
+						unset($languageAliases[$mini]);
+					}
+				}
+			}
+		}
+
+		// add translations to existing server language instance
+		$serverLanguage = $this->getServer()->getLanguage();
+		$refClass = new \ReflectionClass($serverLanguage);
+		$refPropA = $refClass->getProperty('lang');
+		$refPropA->setAccessible(true);
+		/** @var string[] $serverLanguageList */
+		$serverLanguageList = $refPropA->getValue($serverLanguage);
+
+		$pluginLanguage = $languages[$serverLanguage->getLang()];
+		$refClass = new \ReflectionClass($pluginLanguage);
+		$refPropB = $refClass->getProperty('lang');
+		$refPropB->setAccessible(true);
+		/** @var string[] $pluginLanguageList */
+		$pluginLanguageList = $refPropB->getValue($pluginLanguage);
+
+		$refPropA->setValue($serverLanguage, array_merge($serverLanguageList, $pluginLanguageList));
 	}
 }
